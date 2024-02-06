4e2a42f3dfb02c4fc4a078cd729cd59250f4e489
GoldenCTF <136753871+GoldenCTF@users.noreply.github.com>
2024-02-06 14:38:26 +0000

    Add files via upload

---
+++ handler.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_POST['getflag'])) {
+    echo "Gratulerer! Her er flagget ditt: [Du klarte det!]";
+} else {
+    echo "Beklager, du må aktivere knappen for å få flagget.";
+}
+?>
+
---
+++ klikkbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>LOTTO!</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <form name='getflag' class='form' method='post' action='handler.php' id='form1'>
+        <button class='btn block-cube block-cube-hover' id='submit' type='submit' formmethod='post' name='getflag' value='true' disabled>
+        
+        </button>
+    </form>
+</body>
+
+</html>
+
